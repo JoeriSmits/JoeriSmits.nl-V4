d1bcd47ff5a6242d2a6bde7c722d6520a738cd43
Joeri Smits <joeri.smits@planet.nl>
2015-11-24 23:32:38 +0100

    Changed the overal structure of the website

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,6 @@
+/**
+ * Copyright JoeriSmits.nl 2015
+ */
 (function(){
     /**
       * Conditionally load of webcomponentsJS
@@ -12,5 +15,14 @@
         script.async = true;
         script.src = 'bower_components/webcomponentsjs/webcomponents-lite.min.js';
         document.head.appendChild(script);
+    } else {
+        setTimeout(function() {
+            if (window.HTMLImports) {
+                window.CustomElements.elapsed = window.CustomElements.readyTime - window.HTMLImports.readyTime;
+            }
+            document.dispatchEvent(new CustomEvent('WebComponentsReady', {
+                bubbles: true
+            }));
+        });
     }
 })()
